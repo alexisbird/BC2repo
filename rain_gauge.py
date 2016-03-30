b0e93e235f4128cdbb0fb422ad0d5f2e0f7586d5
Alexis <anbird89@gmail.com>
2016-03-30 16:06:49 -0700

    so far this just contains assignment instructions

---
+++ rain_gauge.py
@@ -0,0 +1,65 @@
+"""Practice: Rain
+
+The city of Portland has rain gauges that keep track of rainfall.
+
+[A website](http://or.water.usgs.gov/non-usgs/bes/) has text data tables the history of all the daily measurements at these gauges.
+
+Python gives you a module called `urllib.request` you can use a function `urllib.request.urlopen(url)` which returns a file-like object.
+
+Look at [the docs](https://docs.python.org/3/library/urllib.request.html#module-urllib.request) for that module.
+
+One little difference is the file-like object doesn't return strings, it returns **bytes**.
+
+Use the following code snippet to make it return strings.
+
+```python
+
+import urllib.request
+
+​
+
+with urllib.request.urlopen('http://www.gutenberg.org/ebooks/1342.txt.utf-8') as pride_and_prejudice_file:
+
+  lines = [byte_line.decode('utf-8') for byte_line in pride_and_prejudice_file]
+
+```
+
+​
+
+Use that module to read [the history of the gauge at Sunnyside School](http://or.water.usgs.gov/non-usgs/bes/sunnyside.rain). It looks like:
+
+TEXT HEADER...
+
+​
+
+            Daily  Hourly data -->
+
+   Date     Total    0   1
+
+--------------------------
+
+25-MAR-2016     0    0   0
+
+24-MAR-2016     6    0   1
+
+MORE...
+
+The amounts are in hundredths of inches.
+
+
+* Parse and store the **daily total** data for each day.
+
+Find and print out the specific date with the most rain.
+
+* Find and print out the year with the most rain.
+
+​
+
+## Advanced
+
+* Find and print the day of the year with the most rain on average.
+
+E.g. December 30th has 1" of rain on average.
+
+* Allow someone to type in a date in the future and, using the average value predict the amount of rain."""
+
