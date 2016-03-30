a1b61561e2c0364b8101f0e2392730873d3d7126
Alexis <anbird89@gmail.com>
2016-03-30 12:19:00 -0700

    Fully functioning die stats program - shows die rolled specified number of times, mean, min, and max values. Need to comment still and delete old code

--- die_stats.py
+++ die_stats.py
@@ -11,26 +11,44 @@ from random import randint
 # Variable to store user's input for how many dice they want to roll
 number_of_dice = int(input("How many dice would you like to roll? "))
 total_dice = number_of_dice
-sum_of_dice = 0
 
-while 0 < number_of_dice:
-	die_roll = randint(1, 6)
-	print("Dice rolled: ", die_roll)
+# while 0 < number_of_dice:
+# 	die_roll = randint(1, 6)
+# 	print("Dice rolled: ", die_roll)
 	
-	sum_of_dice += die_roll
-	# Decrement number of dice so while loop knows to stop
-	number_of_dice -= 1
-print("Total sum of dice rolled: ", sum_of_dice)
-mean_of_rolls = sum_of_dice / total_dice
-print("Mean: ", mean_of_rolls)
+# 	sum_of_dice += die_roll
+# 	roll_list = [print(roll) for roll in die_roll]
+# 	print(roll_list)
+# 	# Decrement number of dice so while loop knows to stop
+# 	number_of_dice -= 1
+# print("Total sum of dice rolled: ", sum_of_dice)
+# mean_of_rolls = sum_of_dice / total_dice
+# print("Mean: ", mean_of_rolls)
 # min_roll_num = min(die_roll_list)
 # print("Minimum roll: ", min_roll_num)
 # max_roll_num = 
 
+# roll_list = list(range(1, (total_dice + 1)))
+# print(roll_list)
 
+roll_list = [randint(1, 6) for roll in range(1, (total_dice + 1))]
+print("Here are the numbers you rolled: ", roll_list)
 
+sum_of_rolls = 0
 
+while total_dice > 0:
+	sum_of_rolls += roll_list[total_dice - 1]
+	total_dice -= 1
+	mean_of_rolls = sum_of_rolls / number_of_dice
 
+print("The sum of the rolls is: ", sum_of_rolls)
+print("The mean of the rolls is: ", mean_of_rolls)
+
+min_roll_num = min(roll_list)
+print("The smallest number rolled was ", min_roll_num)
+
+max_roll_num = max(roll_list)
+print("The highest number rolled was ", max_roll_num)
 
 
 
