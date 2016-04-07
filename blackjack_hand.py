0834d66d2975c59ecbcb5d2b886372a116f93ed0
Alexis <anbird89@gmail.com>
2016-04-06 17:58:24 -0700

    Made some progress in terms of solidifying classes so they actually work

--- blackjack_hand.py
+++ blackjack_hand.py
@@ -1,48 +1,46 @@
 """ Create a program that scores a hand of Black Jack """
 
+face_value_dict = {"Ace": 1, "Two": 2, "Three": 3, "Four": 4, "Five": 5, "Six": 6, "Seven": 7, "Eight": 8, "Nine": 9, "Ten": 10, "Jack": 10, "Queen": 10, "King": 10}
+
+suit_list = ["Hearts", "Clubs", "Spades", "Diamonds"]
+
 # 1. Make a Class that represents a card.
 class Card:
 
-	def __init__(self, suit, face, value):
+	def __init__(self, suit, face):
 
 		self.suit = suit
 		self.face = face
-		self.value = value
-
-	def determine_card_value(self):
-		
 		
 
 	def __repr__(self):
-		return self.suit and self.face
+		# You can only technically return one thing, so the way I had it written before with an and statement (return self.face and self.suit) didn't work because Python will only return one of those. Use .format here instead to be able to include multiple attributes of the Class. The squiggly brackets (a.k.a. curly braces) each represent individually represent the arguments in the parentheses after the .format. Thus, self.face appears within the string where the first set of curly braces are and then self.suit appears where the second set of curly braces are. There is an "of" within the string just so it prints out all pretty and proper for these specific circumstances, meaning because it is a card, it will say Queen of Hearts instead of just Queen Hearts. 
+		return "{} of {}".format(self.face, self.suit)
 
 # 2. Make a Class that represents a hand.
 class Hand:
 
-	def __init__(self, card, card_hand):
+	def __init__(self):
 
-		self.card = Card()
-		self.card_hand_list = [Card()]
+		# self.card = Card()
+		self.card_hand_list = []
 
 	# Function that adds a card to a hand
-	def add_card(self):
-		current_card_list = self.card_hand_list.append(self.card)
+	def add_card(self, card):
+		self.card_hand_list.append(card)
 		# Print statement for the time being for testing purposes
-		print(current_card_list)
 
 
 	def __repr__(self):
 
-		return self.card_hand_list
+		return str(self.card_hand_list)
 
 # Function that scores a hand
 def score_hand():
-	face_value_dict = {"Ace": 1, "Two": 2, "Three": 3, "Four": 4, "Five": 5, "Six": 6, "Seven": 7, "Eight": 8, "Nine": 9, "Ten": 10, "Jack": 10, "Queen": 10, "King": 10}
-	suit_list = ["Hearts", "Clubs", "Spades", "Diamonds"]
+	pass
 
 # Function that alerts that hand is now over 21
 def over_21_alert():
-
 	pass
 
 
@@ -54,3 +52,13 @@ def over_21_alert():
 # 4. Allow a user to type in a hand and have it be converted into card objects and then scored
 
 # I think I will need a function that randomly selects a card for the user. Or wait...is that getting ahead of myself and that would be a function once I create a deck of cards
+a_card = Card("Hearts", "Queen")
+print(a_card)
+
+a_hand = Hand()
+print(a_hand)
+
+a_hand.add_card(a_card)
+print(a_hand)
+
+
